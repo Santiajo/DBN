1f34fa519746ee09b20f1c84d935473a5d49c9df
bhuentemilla-nomed <horizonmckay0@gmail.com>
2025-11-08 06:11:22 -0300

    Cambios:
    Se arreglo la logica del bonus proficiencia agregando una nueva pagina para que
    el calculo de oro del trabajo este completo.
    Se cambio el componente checkbox

--- frontend/src/components/checkbox.tsx
+++ frontend/src/components/checkbox.tsx
@@ -3,18 +3,32 @@
 import { InputHTMLAttributes } from 'react';
 
 interface CheckboxProps extends InputHTMLAttributes<HTMLInputElement> {
-  label: string;
+  label?: string; 
   className?: string;
 }
 
 export default function Checkbox({ label, className = '', ...props }: CheckboxProps) {
-  return (
-    <label className={`flex items-center gap-2 cursor-pointer font-body ${className}`}>
+
+  const checkboxInput = (
+    <input
+      type="checkbox"
+      className="h-5 w-5 text-madera-oscura border-madera-oscura rounded focus:ring-2 focus:ring-[#3E6B5C] bg-transparent accent-bosque"
+      {...props}
+    />
+  );
+
+  if (!label) {
+    return (
       <input
         type="checkbox"
-        className="h-5 w-5 text-madera-oscura border-madera-oscura rounded focus:ring-2 focus:ring-[#3E6B5C] bg-transparent accent-bosque"
+        className={`h-5 w-5 text-madera-oscura border-madera-oscura rounded focus:ring-2 focus:ring-[#3E6B5C] bg-transparent accent-bosque ${className}`}
         {...props}
       />
+    );
+  }
+  return (
+    <label className={`flex items-center gap-2 cursor-pointer font-body ${className}`}>
+      {checkboxInput}
       <span className="text-stone-800 select-none">{label}</span>
     </label>
   );
